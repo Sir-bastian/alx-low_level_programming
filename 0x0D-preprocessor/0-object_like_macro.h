ebe427632123a4d1aa2b055c64cf90e8bbb37d33
Sir-bastian <sebamadziwanzira@gmail.com>
2023-04-17 12:26:05 -0700

    0-object_like_macro.h

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,16 +1,6 @@
-#include "0-object_like_macro.h"
-#include <stdio.h>
-/**
- * min - check code
- *
- * Return: Always 0
- */
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
 #define SIZE 1024
 
-int main ()
-{
-	int x;
-	x = SIZE;
-
-	return 0;
-}
+#endif
