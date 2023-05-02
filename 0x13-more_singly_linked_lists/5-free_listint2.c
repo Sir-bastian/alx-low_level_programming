ea1f7a1b6bd2a409ecb3c3752ce2defd52865e60
Sir-bastian <sebamadziwanzira@gmail.com>
2023-05-02 04:25:46 -0700

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -16,12 +16,12 @@ void free_listint2(listint_t **head)
 		return;
 	}
 
-	while (head)
+	while (*head)
 	{
 		temp = (*head);
-		*head = (*head)->next;
-		free(temp);
-		temp = NULL;
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
 	}
 	head = NULL;
 }
